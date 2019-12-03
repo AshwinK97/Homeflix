d54f2c4d0d2924219c31bf96819ebd2913bb57ba
Kaushal Patel <kaushal197@gmail.com>
2019-12-02 22:30:58 -0500

    fixed node modules bug

--- client/src/main.js
+++ client/src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
-import Chat from 'vue-beautiful-chat'
 
 Vue.config.productionTip = false
-Vue.use(Chat);
 
 let globalData = new Vue({
   data: { $userId: '' }
